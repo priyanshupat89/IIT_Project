7c74c1f1b2ee5216b865cf7aa010b33e7c02b0dc
priyanshupat89 <156890827+priyanshupat89@users.noreply.github.com>
2024-02-28 12:06:38 +0530

    Add files via upload

---
+++ Face_Detection Using OpenCV.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+pip install opencv-python
+
+
+# In[1]:
+
+
+import cv2
+
+
+# In[3]:
+
+
+cap = cv2.VideoCapture(0)
+faseCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+while(True):
+    ret,frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faseCascade.detectMultiScale(
+       gray,
+       scaleFactor =1.1,
+       minNeighbors=5,
+       minSize=(30,30)
+     )
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    
+    cv2.imshow("frame",frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
+
